67ac1ac8a3df2ef25182c78f5c6b24aab2c1be7a
amondocorre <amondocorre@capressocafe.com>
2025-07-23 14:46:01 -0400

    hooks v6

--- hotelBackend/application/hooks/nocache.php
+++ hotelBackend/application/hooks/nocache.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class nocache {
+class Nocache {
     function set_no_cache_headers() {
         header("Cache-Control: no-cache, no-store, must-revalidate");
         header("Pragma: no-cache");
